20a380c983bf3aa4d65c16d543732df1ce59b1d3
Hung Nguyen <hung.nguyen@student.tuni.fi>
2020-02-14 11:46:44 +0200

    Add Stage 3 and 4

--- etl/dags/github_trends_hackernews.py
+++ etl/dags/github_trends_hackernews.py
@@ -24,11 +24,14 @@ default_args = {
 }
 
 BQ_CONN = "gcp_conn"
+GCP_PROJECT = "airflow-hackernews-github"
+PROJECT_DATASET = "github_trends"
+
 schedule_interval = "00 21 * * *"
 
 dag = DAG('bigquery_github_trends', default_args = default_args, schedule_interval = schedule_interval)
 
-#Task 1: Check if githubarchive contains data for the date
+#Task 1: Check if githubarchive contains data for the date.
 # To test: docker-compose run --rm webserver airflow test bigquery_github_trends github_table_check 2020-01-01
 t1 = BigQueryCheckOperator(
     task_id = "github_table_check",
@@ -42,10 +45,10 @@ t1 = BigQueryCheckOperator(
     dag = dag
 )
 
-#Task 2: Check if hackernews 
-# To test: docker-compose run --rm webserver airflow test bigquery_github_trends hackernews_table_check 2020-01-01
+#Task 2: Check if hackernews has data for the date.
+# To test: docker-compose run --rm webserver airflow test bigquery_github_trends hackernews_full_table_check 2020-01-01
 t2 = BigQueryCheckOperator(
-    task_id = "hackernews_table_check",
+    task_id = "hackernews_full_table_check",
     sql = """
         SELECT timestamp FROM
         `bigquery-public-data.hacker_news.full`
@@ -57,3 +60,72 @@ t2 = BigQueryCheckOperator(
     use_legacy_sql=False,
     dag=dag
 )
+
+#Task 3: Create/Load github_daily_metrics table
+# To test: docker-compose run --rm webserver airflow test bigquery_github_trends write_to_github_daily_metrics 2020-01-01
+t3 = BigQueryOperator(
+    task_id='write_to_github_daily_metrics',
+    use_legacy_sql=False,
+    write_disposition='WRITE_TRUNCATE',
+    allow_large_results=True,
+    sql='''
+    #standardSQL
+    SELECT
+      date,
+      repo,
+      SUM(IF(type='WatchEvent', 1, NULL)) AS stars,
+      SUM(IF(type='ForkEvent',  1, NULL)) AS forks
+    FROM (
+      SELECT
+        FORMAT_TIMESTAMP("%Y%m%d", created_at) AS date,
+        actor.id as actor_id,
+        repo.name as repo,
+        type
+      FROM
+        `githubarchive.day.{{ yesterday_ds_nodash }}`
+      WHERE type IN ('WatchEvent','ForkEvent')
+    )
+    GROUP BY
+      date,
+      repo
+    ''',    
+    destination_dataset_table=f'{GCP_PROJECT}.{PROJECT_DATASET}.github_daily_metrics{r"${{ yesterday_ds_nodash }}"}',
+    bigquery_conn_id=BQ_CONN,
+    dag=dag
+)
+
+# Task 4: Aggregate Github metrics into the last 1, 7, 28 days.
+# To test: docker-compose run --rm webserver airflow test bigquery_github_trends aggregate_github_metrics 2020-01-01
+t4 = BigQueryOperator(
+    task_id='aggregate_github_metrics',
+    use_legacy_sql=False,
+    write_disposition='WRITE_TRUNCATE',
+    allow_large_results=True,
+    sql=f'''
+    #standardSQL
+    SELECT
+        {r"{{ yesterday_ds_nodash }}"} as date,
+        repo, 
+        SUM(IF(_PARTITIONTIME BETWEEN TIMESTAMP("{r"{{ yesterday_ds }}"}") 
+            AND TIMESTAMP("{r"{{ yesterday_ds }}"}"), stars, NULL)) AS stars_last_1_day,
+        SUM(IF(_PARTITIONTIME BETWEEN TIMESTAMP("{r"{{ macros.ds_add(ds, -6) }}"}") 
+            AND TIMESTAMP("{r"{{ yesterday_ds }}"}"), stars, NULL)) AS stars_last_7_days,
+        SUM(IF(_PARTITIONTIME BETWEEN TIMESTAMP("{r"{{ macros.ds_add(ds, -27) }}"}") 
+            AND TIMESTAMP("{r"{{ yesterday_ds }}"}"), stars, NULL)) AS stars_last_28_days,
+        SUM(IF(_PARTITIONTIME BETWEEN TIMESTAMP("{r"{{ yesterday_ds }}"}") 
+            AND TIMESTAMP("{r"{{ yesterday_ds }}"}"), forks, NULL)) AS forks_last_1_day,
+        SUM(IF(_PARTITIONTIME BETWEEN TIMESTAMP("{r"{{ macros.ds_add(ds, -6) }}"}") 
+            AND TIMESTAMP("{r"{{ yesterday_ds }}"}"), forks, NULL)) AS forks_last_7_days,
+        SUM(IF(_PARTITIONTIME BETWEEN TIMESTAMP("{r"{{ macros.ds_add(ds, -27) }}"}") 
+            AND TIMESTAMP("{r"{{ yesterday_ds }}"}"), forks, NULL)) AS forks_last_28_days
+    FROM `{GCP_PROJECT}.{PROJECT_DATASET}.github_daily_metrics`
+    WHERE _PARTITIONTIME BETWEEN TIMESTAMP("{r"{{ macros.ds_add(ds, -27) }}"}") AND TIMESTAMP("{r"{{ yesterday_ds }}"}")
+    GROUP BY
+      date,
+      repo
+    ''',    
+    destination_dataset_table=f'{GCP_PROJECT}.{PROJECT_DATASET}.github_agg${r"{{ yesterday_ds_nodash }}"}',
+    bigquery_conn_id=BQ_CONN,
+    dag=dag
+)
+
