8c43dbdc742a66d042aba9373591cfc97d8f4413
Hung Nguyen <hungnguyen10897@gmail.com>
2020-02-14 19:01:10 +0200

    Add step 6 and 7, finished and tested all steps

--- etl/dags/github_trends_hackernews.py
+++ etl/dags/github_trends_hackernews.py
@@ -163,3 +163,68 @@ t5 = BigQueryOperator(
     dag=dag
 )
 
+
+# Task 6: Join the GitHub and HackerNews aggregated table
+# To test: docker-compose run --rm webserver airflow test bigquery_github_trends hackernews_github_join 2020-01-01
+t6 = BigQueryOperator(
+    task_id='hackernews_github_join',
+    use_legacy_sql=False,
+    write_disposition='WRITE_TRUNCATE',
+    allow_large_results=True,
+    bql=f'''
+    #standardSQL
+    SELECT
+      a.date as date,
+      a.url as github_url,
+      b.repo as github_repo,
+      a.score as hn_score,
+      a.story_id as hn_story_id,
+      b.stars_last_28_days as stars_last_28_days,
+      b.stars_last_7_days as stars_last_7_days,
+      b.stars_last_1_day as stars_last_1_day,
+      b.forks_last_28_days as forks_last_28_days,
+      b.forks_last_7_days as forks_last_7_days,
+      b.forks_last_1_day as forks_last_1_day
+    FROM
+    (SELECT
+      *
+    FROM
+      `{GCP_PROJECT}.{PROJECT_DATASET}.hackernews_agg`
+      WHERE _PARTITIONTIME BETWEEN TIMESTAMP("{r"{{ yesterday_ds }}"}") AND TIMESTAMP("{r"{{ yesterday_ds }}"}")
+      )AS a
+    LEFT JOIN
+      (
+      SELECT
+        repo,
+        CONCAT('https://github.com/', repo) as url,
+        stars_last_28_days,
+        stars_last_7_days,
+        stars_last_1_day,
+        forks_last_28_days,
+        forks_last_7_days,
+        forks_last_1_day
+      FROM
+      `{GCP_PROJECT}.{PROJECT_DATASET}.github_agg`
+      WHERE _PARTITIONTIME BETWEEN TIMESTAMP("{r"{{ yesterday_ds }}"}") AND TIMESTAMP("{r"{{ yesterday_ds }}"}")
+      ) as b
+    ON a.url = b.url
+    ''',
+    destination_dataset_table=f'{GCP_PROJECT}.{PROJECT_DATASET}.hackernews_github_agg${r"{{ yesterday_ds_nodash }}"}',
+    bigquery_conn_id= BQ_CONN,
+    dag=dag)
+
+# Task 7: Check for data in the final table
+# To test: docker-compose run --rm webserver airflow test bigquery_github_trends check_hackernews_github_join 2020-01-01
+t7 = BigQueryCheckOperator(
+    task_id='check_hackernews_github_join',
+    sql=f'''
+    #legacySql
+    SELECT
+    partition_id
+    FROM
+    [{GCP_PROJECT}:{PROJECT_DATASET}.hackernews_github_agg$__PARTITIONS_SUMMARY__]
+    WHERE partition_id = "{r"{{ yesterday_ds_nodash }}"}"
+    ''',
+    bigquery_conn_id= BQ_CONN,
+    dag=dag)
+
